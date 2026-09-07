3da2c7de55573da74e2c3f0fcd1c6aa32c2d2c90
montmurray <vinylscratch1103@gmail.com>
2026-09-07 18:46:27 -0400

    Man I suck at this, fixing conditionals again and typecasting n.

--- src/main/java/DEMOP2.java
+++ src/main/java/DEMOP2.java
@@ -14,18 +14,25 @@ public class DEMOP2 {
     System.out.println("Hello " + name + ". Nice to know you're literate.");
     System.out.println("But are you mathematical? Give me a number.");
 
-    Object n = s.next();
+    Object n;
     while (s.hasNext()) {
       if (s.hasNextInt()) {
+        n = s.nextInt();
         System.out.println(n + "? Great choice. I like you.");
         break;
       } else if (s.hasNextDouble() || s.hasNextFloat()) {
+        if (s.hasNextDouble()) {
+          n = s.nextDouble();
+        } else {
+          n = s.nextFloat();
+        }
         System.out.println(n + "? Woah, you're better than I thought. You've got some knowledge");
         break;
-      } else System.out.println("That's not a number, don't mess up this time.");
-      // no, I am not using a logpoint.
-      // Java is just Diet Swift woah.
+      } else {
+        System.out.println("That's not a number, don't mess up this time.");
+        s.next();
+      }
     }
     System.out.println("Oh look, the programmer figured it out. Bare minimum yay.");
-    }
+  }
 }
