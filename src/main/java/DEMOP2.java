8b2c8c4ac02bfbdf4fa549dc747e3518f656354b
montmurray <vinylscratch1103@gmail.com>
2026-09-07 18:38:32 -0400

    Fixed conditionals, added confirmation to assure I know what I'm doing here.

--- src/main/java/DEMOP2.java
+++ src/main/java/DEMOP2.java
@@ -2,31 +2,30 @@ import java.util.Scanner;
 import java.util.Objects;
 
 public class DEMOP2 {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+  public static void main(String[] args) {
+    System.out.println("Hello World");
 
-        System.out.println("Wait---I wasn't here before...");
+    System.out.println("Wait---I wasn't here before...");
 
-        System.out.println("Who are YOU? Whats YOUR name?");
-        Scanner s = new Scanner(System.in);
+    System.out.println("Who are YOU? Whats YOUR name?");
+    Scanner s = new Scanner(System.in);
 
-        String name = s.nextLine();
-        System.out.println("Hello " + name + ". Nice to know you're literate.");
-        System.out.println("But are you mathematical? Give me a number.");
+    String name = s.nextLine();
+    System.out.println("Hello " + name + ". Nice to know you're literate.");
+    System.out.println("But are you mathematical? Give me a number.");
 
-        Object n = s.next();
-        while (s.hasNext()) {
-          if (n instanceof Integer) {
-                System.out.println(n + "? Great choice. I like you.");
-              } else if (n instanceof Double) {
-                System.out.println(n + "? Woah, you're better than I thought. You've got some knowledge");
-              } else {
-                System.out.println("That's not a number, don't mess up this time.");
-              }
-        }
-        //no, I am not using a logpoint.
-        //Java is just Diet Swift woah.
-
-
+    Object n = s.next();
+    while (s.hasNext()) {
+      if (s.hasNextInt()) {
+        System.out.println(n + "? Great choice. I like you.");
+        break;
+      } else if (s.hasNextDouble() || s.hasNextFloat()) {
+        System.out.println(n + "? Woah, you're better than I thought. You've got some knowledge");
+        break;
+      } else System.out.println("That's not a number, don't mess up this time.");
+      // no, I am not using a logpoint.
+      // Java is just Diet Swift woah.
+    }
+    System.out.println("Oh look, the programmer figured it out. Bare minimum yay.");
     }
 }
