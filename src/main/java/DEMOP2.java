0c81a861fafdaae812fe055bbb564f56eabff42a
montmurray <vinylscratch1103@gmail.com>
2026-09-09 22:54:22 -0400

    added the comments back, no change.

--- src/main/java/DEMOP2.java
+++ src/main/java/DEMOP2.java
@@ -1,40 +1,46 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
 import java.util.Scanner;
-import java.util.Objects;
 
 public class DEMOP2 {
-  public static void main(String[] args) {
-    System.out.println("Hello World");
-
-    System.out.println("Wait---I wasn't here before...");
-
-    System.out.println("Who are YOU? Whats YOUR name?");
-    Scanner s = new Scanner(System.in);
-
-    String name = s.nextLine();
-    System.out.println("Hello " + name + ". Nice to know you're literate.");
-    System.out.println("But are you mathematical? Give me a number.");
-
-    Object n;
-    while (s.hasNext()) {
-      if (s.hasNextInt()) {
-        n = s.nextInt();
-        System.out.println(n + "? Great choice. I like you.");
-        break;
-      } else if (s.hasNextDouble() || s.hasNextFloat()) {
-        if (s.hasNextDouble()) {
-          n = s.nextDouble();
-        } else {
-          n = s.nextFloat();
-        }
-        System.out.println(n + "? Woah, you're better than I thought. You've got some knowledge");
-        break;
-      } else {
-        System.out.println("That's not a number, don't mess up this time.");
-        s.next();
-      }
+    public DEMOP2() {
     }
-    System.out.println("Oh look, the programmer figured it out. Bare minimum yay.");
-  }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Wait---I wasn't here before...");
+        System.out.println("Who are YOU? Whats YOUR name?");
+        Scanner s = new Scanner(System.in);
+        String name = s.nextLine();
+        System.out.println("Hello " + name + ". Nice to know you're literate.");
+        System.out.println("But are you mathematical? Give me a number.");
+
+        while(s.hasNext()) {
+            if (s.hasNextInt()) {
+                Object n = s.nextInt();
+                System.out.println(n + "? Great choice. I like you.");
+                break;
+            }
+
+            if (s.hasNextDouble() || s.hasNextFloat()) {
+                Object n;
+                if (s.hasNextDouble()) {
+                    n = s.nextDouble();
+                } else {
+                    n = s.nextFloat();
+                }
+
+                System.out.println(n + "? Woah, you're better than I thought. You've got some knowledge");
+                break;
+            }
+
+            System.out.println("That's not a number, don't mess up this time.");
+            s.next();
+        }
+
+        System.out.println("Oh look, the programmer figured it out. Bare minimum yay.");
+    }
 }
-//I learned Swift in senior year of HS, found out Swift is just Diet Java.
-//Stop telling me to write a logpoint for sys.out I don't want to.
